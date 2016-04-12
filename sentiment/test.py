e958aaedd91446f6860bbcd88f27c0d9b6f5b5d2
Ignas Usinskas <Ignas Usinskas>
2016-04-12 15:40:42 +0100

    clean up files

--- sentiment/test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-from mpld3 import fig_to_html, plugins
-fig, ax = plt.subplots()
-points = ax.plot(range(10), 'o')
-plugins.connect(fig, plugins.PointLabelTooltip(points[0]))
-fig_to_html(fig)
